9b0dcbd050b95ec08cefe20f098c4f09b89d1a68
MorrisGcode <morrisgithinji07@gmail.com>
2023-03-21 12:25:37 +0000

    edit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alphabet in range(26):
-    if alphabet != 4 and alphabet != 16:
-        print("{:s}".format(chr(alphabet + ord("a"))), end="")
+	 if alphabet != 4 and alphabet != 16:
+		 print("{:s}".format(chr(alphabet + ord("a"))), end="")
 
