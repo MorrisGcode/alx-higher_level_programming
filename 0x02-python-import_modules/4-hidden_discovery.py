a4ef20552362e58b1d782642bd369e3763747baa
MorrisGcode <morrisgithinji07@gmail.com>
2023-04-11 15:17:06 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import hidden_4
 if __name__ == "__main__":
      """Print all names in the hidden_4 module."""
+    import hidden_4
  
     for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
