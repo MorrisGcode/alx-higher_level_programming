cea08cfb76b2cd1f8682295f398abbcb396a3c54
MorrisGcode <morrisgithinji07@gmail.com>
2023-04-25 09:29:25 +0000

    edit function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_lst = []
-    if len(matrix) == 0:
-        return new_lst
-
-    new_lst = [[i*i for i in j] for j in matrix]
-    return new_list
+    new_list = [[i*i for i in j] for j in matrix]
+    return(new_list)
